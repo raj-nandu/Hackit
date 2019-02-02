f7bb3ce02680f7708f65793656bd6155b2f608c2
Amey <amey.meher@somaiya.edu>
2019-02-02 16:16:03 +0530

    Status bar flicker and vibrate added

--- ThinkBlink/app/src/main/java/projects/amey/com/thinkblink/MainActivity.java
+++ ThinkBlink/app/src/main/java/projects/amey/com/thinkblink/MainActivity.java
@@ -1,10 +1,20 @@
 package projects.amey.com.thinkblink;
 
+import android.Manifest;
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -13,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     /*  Permission request code to draw over other apps  */
     private static final int DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE = 1222;
-
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 21;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +65,89 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getString(R.string.draw_other_app_permission_denied),
                         Toast.LENGTH_SHORT).show();
 
+        } else if(requestCode == MY_PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if(resultCode == RESULT_OK)
+                ;
+            else
+                Toast.makeText(this,"Vibrate permission not granted",Toast.LENGTH_LONG).show();
         } else {
-            super.onActivityResult(requestCode, resultCode, data);
+                super.onActivityResult(requestCode, resultCode, data);
+
+        }
+    }
+
+    public void statusBarFlicker(View view){
+        final int statusBarColor = getResources().getColor(R.color.status_bar_color);
+        final int statusBarToColor = getResources().getColor(R.color.status_bar_to_color);
+
+        ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // Use animation position to blend colors.
+                float position = animation.getAnimatedFraction();
+
+                // Apply blended color to the status bar.
+                int blended = blendColors(statusBarColor, statusBarToColor, position);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    changeTheColor(blended);
+                }
+            }
+        });
+
+        anim.setDuration(500).start();
+
+    }
+
+    public void changeTheColor(int blended){
+        getWindow().setStatusBarColor(blended);
+    }
+
+    private int blendColors(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.rgb((int) r, (int) g, (int) b);
+    }
+
+    public void checkVibrationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.VIBRATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.VIBRATE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.VIBRATE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+    }
+
+    public void vibratePhone(View view){
+        checkVibrationPermission();
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(500);
         }
     }
 }
