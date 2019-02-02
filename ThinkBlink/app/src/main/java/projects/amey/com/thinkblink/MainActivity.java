8341d63119e843d0a009dd3f721a7f36cf8d20eb
Amey <amey.meher@somaiya.edu>
2019-02-02 15:24:28 +0530

    Floating widget part 2 full functionality

--- ThinkBlink/app/src/main/java/projects/amey/com/thinkblink/MainActivity.java
+++ ThinkBlink/app/src/main/java/projects/amey/com/thinkblink/MainActivity.java
@@ -3,86 +3,60 @@ package projects.amey.com.thinkblink;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int DRAW_OVER_OTHER_APP_PERMISSION = 123;
-    private Button button;
+    /*  Permission request code to draw over other apps  */
+    private static final int DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE = 1222;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        askForSystemOverlayPermission();
-
-        button = (Button) findViewById(R.id.button);
-        int badge_count = getIntent().getIntExtra("badge_count", 0);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(MainActivity.this)) {
-                    startService(new Intent(MainActivity.this, FloatingWidgetService.class));
-                } else {
-                    errorToast();
-                }
-            }
-        });
-
     }
 
-    private void askForSystemOverlayPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+    /*  start floating widget service  */
+    public void createFloatingWidget(View view) {
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
             //If the draw over permission is not available open the settings screen
             //to grant the permission.
             Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                     Uri.parse("package:" + getPackageName()));
-            startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION);
-        }
+            startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE);
+        } else
+            //If permission is granted start floating widget service
+            startFloatingWidgetService();
+
+    }
+
+    /*  Start Floating widget service and finish current activity */
+    private void startFloatingWidgetService() {
+        startService(new Intent(MainActivity.this, FloatingWidgetService.class));
+        finish();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == DRAW_OVER_OTHER_APP_PERMISSION) {
+        if (requestCode == DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE) {
+            //Check if the permission is granted or not.
+            if (resultCode == RESULT_OK)
+                //If permission granted start floating widget service
+                startFloatingWidgetService();
+            else
+                //Permission is not available then display toast
+                Toast.makeText(this,
+                        getResources().getString(R.string.draw_other_app_permission_denied),
+                        Toast.LENGTH_SHORT).show();
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (!Settings.canDrawOverlays(this)) {
-                    //Permission is not available. Display error text.
-                    errorToast();
-                    finish();
-                }
-            }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
-
-    private void errorToast() {
-        Toast.makeText(this, "Draw over other app permission not available. Can't start the application without the permission.", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-
-        // To prevent starting the service if the required permission is NOT granted.
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)) {
-            startService(new Intent(MainActivity.this, FloatingWidgetService.class).putExtra("activity_background", true));
-            finish();
-        } else {
-            errorToast();
-        }
-    }
 }
-
-
-
