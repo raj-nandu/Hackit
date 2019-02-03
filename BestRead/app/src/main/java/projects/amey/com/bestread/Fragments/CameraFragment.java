b189fb21db21ec137b49e9233cac3a74e814d948
Amey <amey.meher@somaiya.edu>
2019-02-03 08:54:41 +0530

    Final app ka part 3

--- BestRead/app/src/main/java/projects/amey/com/bestread/Fragments/CameraFragment.java
+++ BestRead/app/src/main/java/projects/amey/com/bestread/Fragments/CameraFragment.java
@@ -16,6 +16,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.content.res.AssetManager;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelManager;
+import com.google.firebase.ml.custom.FirebaseModelOptions;
+import com.google.firebase.ml.custom.model.FirebaseCloudModelSource;
+import com.google.firebase.ml.custom.model.FirebaseLocalModelSource;
+import com.google.firebase.ml.custom.model.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 import projects.amey.com.bestread.R;
 
@@ -26,6 +57,53 @@ public class CameraFragment extends Fragment {
     private TextFragment.OnFragmentInteractionListener mListener;
     private ImageView imageView;
     private Button selectFromGalleryButton;
+    private Bitmap mSelectedImage;
+    private String text = "";
+    private Button mTextButton;
+    private List<String> mLabelList;
+    private static final String HOSTED_MODEL_NAME = "cloud_model_1";
+    private static final String LOCAL_MODEL_ASSET = "mobilenet_v1_1.0_224_quant.tflite";
+    /**
+     * Name of the label file stored in Assets.
+     */
+    private static final String LABEL_PATH = "labels.txt";
+    /**
+     * Number of results to show in the UI.
+     */
+    private static final int RESULTS_TO_SHOW = 3;
+    /**
+     * Dimensions of inputs.
+     */
+    private static final int DIM_BATCH_SIZE = 1;
+    private static final int DIM_PIXEL_SIZE = 3;
+    private static final int DIM_IMG_SIZE_X = 224;
+    private static final int DIM_IMG_SIZE_Y = 224;
+    private static int RESULT_LOAD_IMAGE = 1;
+    private static int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
+    /**
+     * Labels corresponding to the output of the vision model.
+     */
+
+    private final PriorityQueue<Map.Entry<String, Float>> sortedLabels =
+            new PriorityQueue<>(
+                    RESULTS_TO_SHOW,
+                    new Comparator<Map.Entry<String, Float>>() {
+                        @Override
+                        public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float>
+                                o2) {
+                            return (o1.getValue()).compareTo(o2.getValue());
+                        }
+                    });
+    /* Preallocated buffers for storing image data. */
+    private final int[] intValues = new int[DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y];
+    /**
+     * An instance of the driver class to run model inference with Firebase.
+     */
+    private FirebaseModelInterpreter mInterpreter;
+    /**
+     * Data configuration of input & output data of model.
+     */
+    private FirebaseModelInputOutputOptions mDataOptions;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +127,14 @@ public class CameraFragment extends Fragment {
         View view = layoutInflater.inflate(R.layout.fragment_camera,container,false);
         imageView = (ImageView) view.findViewById(R.id.imageView1);
         selectFromGalleryButton = (Button) view.findViewById(R.id.button5);
+        mTextButton = (Button) view.findViewById(R.id.button);
+
+        mTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                runTextRecognition();
+            }
+        });
 
         selectFromGalleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,9 +143,240 @@ public class CameraFragment extends Fragment {
                 startActivityForResult(intent, 1);
             }
         });
+
+        initCustomModel();
         return view;
     }
 
+    private void initCustomModel() {
+        mLabelList = loadLabelList(this);
+
+        int[] inputDims = {DIM_BATCH_SIZE, DIM_IMG_SIZE_X, DIM_IMG_SIZE_Y, DIM_PIXEL_SIZE};
+        int[] outputDims = {DIM_BATCH_SIZE, mLabelList.size()};
+        try {
+            mDataOptions =
+                    new FirebaseModelInputOutputOptions.Builder()
+                            .setInputFormat(0, FirebaseModelDataType.BYTE, inputDims)
+                            .setOutputFormat(0, FirebaseModelDataType.BYTE, outputDims)
+                            .build();
+            FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions
+                    .Builder()
+                    .requireWifi()
+                    .build();
+            FirebaseLocalModelSource localSource =
+                    new FirebaseLocalModelSource.Builder("asset")
+                            .setAssetFilePath(LOCAL_MODEL_ASSET).build();
+
+            FirebaseCloudModelSource cloudSource = new FirebaseCloudModelSource.Builder
+                    (HOSTED_MODEL_NAME)
+                    .enableModelUpdates(true)
+                    .setInitialDownloadConditions(conditions)
+                    .setUpdatesDownloadConditions(conditions)  // You could also specify
+                    // different conditions
+                    // for updates
+                    .build();
+            FirebaseModelManager manager = FirebaseModelManager.getInstance();
+            manager.registerLocalModelSource(localSource);
+            manager.registerCloudModelSource(cloudSource);
+            FirebaseModelOptions modelOptions =
+                    new FirebaseModelOptions.Builder()
+                            .setCloudModelName(HOSTED_MODEL_NAME)
+                            .setLocalModelName("asset")
+                            .build();
+            mInterpreter = FirebaseModelInterpreter.getInstance(modelOptions);
+        } catch (FirebaseMLException e) {
+            System.out.println("Error while setting up the model");
+            e.printStackTrace();
+        }
+    }
+
+    private void runTextRecognition() {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        mTextButton.setEnabled(false);
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                                mTextButton.setEnabled(true);
+
+                                processTextRecognitionResult(texts);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                mTextButton.setEnabled(true);
+                                e.printStackTrace();
+                            }
+                        });
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts) {
+        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+        if (blocks.size() == 0) {
+            System.out.println("No text found");
+            return;
+        }
+        //mGraphicOverlay.clear();
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    //Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
+                    //mGraphicOverlay.add(textGraphic);
+
+
+                }
+                text = text + lines.get(j).getText();
+
+            }
+        }
+        System.out.println(text);
+    }
+
+    private synchronized List<String> getTopLabels(byte[][] labelProbArray) {
+        for (int i = 0; i < mLabelList.size(); ++i) {
+            sortedLabels.add(
+                    new AbstractMap.SimpleEntry<>(mLabelList.get(i), (labelProbArray[0][i] &
+                            0xff) / 255.0f));
+            if (sortedLabels.size() > RESULTS_TO_SHOW) {
+                sortedLabels.poll();
+            }
+        }
+        List<String> result = new ArrayList<>();
+        final int size = sortedLabels.size();
+        for (int i = 0; i < size; ++i) {
+            Map.Entry<String, Float> label = sortedLabels.poll();
+            result.add(label.getKey() + ":" + label.getValue());
+        }
+        Log.d("labels", "labels: " + result.toString());
+        return result;
+    }
+
+    /**
+     * Reads label list from Assets.
+     * @param activity
+     */
+    private List<String> loadLabelList(CameraFragment activity) {
+        List<String> labelList = new ArrayList<>();
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(getActivity().getAssets().open
+                             (LABEL_PATH)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                labelList.add(line);
+            }
+        } catch (IOException e) {
+            Log.e("error", "Failed to read label list.", e);
+        }
+        return labelList;
+    }
+
+    /**
+     * Writes Image data into a {@code ByteBuffer}.
+     */
+    private synchronized ByteBuffer convertBitmapToByteBuffer(
+            Bitmap bitmap, int width, int height) {
+        ByteBuffer imgData =
+                ByteBuffer.allocateDirect(
+                        DIM_BATCH_SIZE * DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y * DIM_PIXEL_SIZE);
+        imgData.order(ByteOrder.nativeOrder());
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, DIM_IMG_SIZE_X, DIM_IMG_SIZE_Y,
+                true);
+        imgData.rewind();
+        scaledBitmap.getPixels(intValues, 0, scaledBitmap.getWidth(), 0, 0,
+                scaledBitmap.getWidth(), scaledBitmap.getHeight());
+        // Convert the image to int points.
+        int pixel = 0;
+        for (int i = 0; i < DIM_IMG_SIZE_X; ++i) {
+            for (int j = 0; j < DIM_IMG_SIZE_Y; ++j) {
+                final int val = intValues[pixel++];
+                imgData.put((byte) ((val >> 16) & 0xFF));
+                imgData.put((byte) ((val >> 8) & 0xFF));
+                imgData.put((byte) (val & 0xFF));
+            }
+        }
+        return imgData;
+    }
+
+    // Functions for loading images from app assets.
+
+    //
+//    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+//        mGraphicOverlay.clear();
+//        switch (position) {
+//            case 0:
+//                mSelectedImage = getBitmapFromAsset(this, "Please_walk_on_the_grass.jpg");
+//                break;
+//            case 1:
+//                // Whatever you want to happen when the thrid item gets selected
+//                mSelectedImage = getBitmapFromAsset(this, "nl2.jpg");
+//                break;
+//            case 2:
+//                // Whatever you want to happen when the thrid item gets selected
+//                mSelectedImage = getBitmapFromAsset(this, "grace_hopper.jpg");
+//                break;
+//            case 3:
+//                // Whatever you want to happen when the thrid item gets selected
+//                mSelectedImage = getBitmapFromAsset(this, "tennis.jpg");
+//                break;
+//            case 4:
+//                // Whatever you want to happen when the thrid item gets selected
+//                mSelectedImage = getBitmapFromAsset(this, "mountain.jpg");
+//                break;
+//        }
+//        if (mSelectedImage != null) {
+//            // Get the dimensions of the View
+//            Pair<Integer, Integer> targetedSize = getTargetedWidthHeight();
+//
+//            int targetWidth = targetedSize.first;
+//            int maxHeight = targetedSize.second;
+//
+//            // Determine how much to scale down the image
+//            float scaleFactor =
+//                    Math.max(
+//                            (float) mSelectedImage.getWidth() / (float) targetWidth,
+//                            (float) mSelectedImage.getHeight() / (float) maxHeight);
+//
+//            Bitmap resizedBitmap =
+//                    Bitmap.createScaledBitmap(
+//                            mSelectedImage,
+//                            (int) (mSelectedImage.getWidth() / scaleFactor),
+//                            (int) (mSelectedImage.getHeight() / scaleFactor),
+//                            true);
+//
+//            mImageView.setImageBitmap(resizedBitmap);
+//            mSelectedImage = resizedBitmap;
+//        }
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//        // Do nothing
+//    }
+
+
+
+    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream is;
+        Bitmap bitmap = null;
+        try {
+            is = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -108,6 +425,7 @@ public class CameraFragment extends Fragment {
                     Bitmap bitmap = BitmapFactory.decodeFile(filepath);
                     Drawable drawable = new BitmapDrawable(bitmap);
                     imageView.setBackground(drawable);
+                    mSelectedImage = bitmap;
                 }
                 break;
             default:
